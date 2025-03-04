d47f20af1b56b68dd3e2dd075e4ec7904492a1d9
LuizMafraJNR <luiz.sesna@univille.br>
2025-03-04 14:05:46 -0300

    Aula 23

---
+++ src/main/java/com/mafra/hexagonal/adapters/in/consumer/ReceiveValidatedCpfConsumer.java
@@ -0,0 +1,24 @@
+package com.mafra.hexagonal.adapters.in.consumer;
+
+import com.mafra.hexagonal.adapters.in.consumer.mapper.CustomerMessageMapper;
+import com.mafra.hexagonal.adapters.in.consumer.message.CustomerMessage;
+import com.mafra.hexagonal.application.ports.in.UpdateCustomerInputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReceiveValidatedCpfConsumer {
+
+    @Autowired
+    private UpdateCustomerInputPort updateCustomerInputPort;
+
+    @Autowired
+    private CustomerMessageMapper customerMessageMapper;
+
+    @KafkaListener(topics = "tp-cpf-validated", groupId = "arantes")
+    public void receive(CustomerMessage customerMessage){
+        var custoomer = customerMessageMapper.toCustomer(customerMessage);
+        updateCustomerInputPort.update(custoomer,customerMessage.getZipCode());
+    }
+}
